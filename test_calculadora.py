02c7fcf5af10e15228d757086f03f08d12debe06
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 20:06:35 -0300

    refeito teste de parametro invalido

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,7 +3,16 @@ import pytest
 def multiplica(valor1 = 0, valor2 = 0):
     valor1 = valor1 or 0
     valor2 = valor2 or 0
+
+    valorUmNaoEhUmNumero = valor1.isnumeric()
+    valorDoisNaoEhUmNumero = valor2..isnumeric()
+
+    if(valorUmNaoEhUmNumero):
+        raise Exception('valor1 nao eh um numero')
     
+    if(valorUmNaoEhUmNumero):
+        raise Exception('valor2 nao eh um numero')
+
     resultado_multiplicacao = valor1 * valor2
     
     return resultado_multiplicacao
@@ -13,6 +22,6 @@ def test_multiplica_com_parametros_validos(valor1, valor2, valor_esperado):
     assert valor1 * valor2 == valor_esperado
     assert multiplica(valor1, valor2) == valor_esperado
 
-@pytest.mark.parametrize('valor_invalido1, valor_invalido2, valor_esperado', [(None, None, 0), ('um', 'dois', 'dois')])
-def test_multiplica_com_parametros_invalidos(valor_invalido1, valor_invalido2, valor_esperado):
-    assert multiplica(valor_invalido1, valor_invalido2) == valor_esperado
+def test_multiplica_com_parametros_invalidos(valor_invalido1, valor_invalido2):
+    with pytest.raises(Exception):
+        multiplica(valor_invalido1, valor_invalido2) == valor_esperado
