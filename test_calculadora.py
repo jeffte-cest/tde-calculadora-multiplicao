8605ba5ef9476712569d91d2b0e96b5229ebe553
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 20:23:57 -0300

    teste chamada funcao multiplica com valor invalido

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,6 +1,11 @@
 import pytest
 
 def multiplica(valor1, valor2):
+    isParametroUmNumerico = type(valor1) is int or type(valor1) is float
+
+    if(not isParametroUmNumerico):
+        raise Exception("parametro 1 eh invalido")
+
     resultado_multiplicacao = valor1 * valor2
     
     return resultado_multiplicacao
