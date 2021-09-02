5bda9123c7a61aaddbb0b258d8b0462921f70d2a
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:37:56 -0300

    adicionado variavel valor esperado

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,10 +5,10 @@ def multiplica(valor1, valor2):
 
 
 @pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
-def test_multiplica(valor1, valor2):
+def test_multiplica(valor1, valor2, valor_esperado):
     assert multiplica(valor1, valor1) == valor_esperado
 
 @pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
-def test_multiplica_com_parametros_validos(valor1, valor2):
+def test_multiplica_com_parametros_validos(valor1, valor2, valor_esperado):
     assert valor1 * valor2 == valor_esperado
     assert multiplica(valor1, valor2) == valor_esperado
