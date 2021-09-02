0b01f28b92fb70c120b8d390ce457f0d098470b9
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:41:29 -0300

    teste chamada da funcao com parametros validos se 1 x 1 e igual a 1

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,7 +3,7 @@ import pytest
 def multiplica(valor1, valor2):
     return 0
 
-@pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
+@pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0), [1, 1, 1]])
 def test_multiplica_com_parametros_validos(valor1, valor2, valor_esperado):
     assert valor1 * valor2 == valor_esperado
     assert multiplica(valor1, valor2) == valor_esperado
