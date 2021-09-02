0752491d67ea4e5fb71a47c7c8f84ec410891f92
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:39:22 -0300

    removido teste criado para testar o pytest

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,11 +3,6 @@ import pytest
 def multiplica(valor1, valor2):
     return 0
 
-
-@pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
-def test_multiplica(valor1, valor2, valor_esperado):
-    assert multiplica(valor1, valor1) == valor_esperado
-
 @pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
 def test_multiplica_com_parametros_validos(valor1, valor2, valor_esperado):
     assert valor1 * valor2 == valor_esperado
