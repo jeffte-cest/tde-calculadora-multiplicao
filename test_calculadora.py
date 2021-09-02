d50199eeb91e6129996e0608c9a4d76f70513bdd
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:32:12 -0300

    alterado nome das variaveis

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,10 +1,10 @@
 import pytest
 
-def multiplica(num1, num2):
+def multiplica(valor1, valor2):
     return 0
 
 
-@pytest.mark.parametrize('num1, num2, valoresperado', [(0, 0, 0)])
-def test_multiplica(num1, num2, valoresperado):
-    assert multiplica(num1, num2) == valoresperado
+@pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
+def test_multiplica(valor1, valor2, valor_esperado):
+    assert multiplica(valor1, valor1) == valor_esperado
 
