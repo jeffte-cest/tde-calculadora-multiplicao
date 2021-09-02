296503258154501b40593241ad9fea5881911470
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 20:32:08 -0300

    refatorado funcao para verificar tipo do parametro2

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,6 +6,9 @@ def multiplica(valor1, valor2):
 
     if(not isParametroUmNumerico):
         raise Exception("parametro 1 eh invalido")
+    
+    if(not isParametroDoisNumerico):
+        raise Exception("parametro 2 eh invalido")
 
     resultado_multiplicacao = valor1 * valor2
     
