1587dc496351c398f88a4d1022d5b3e612e55281
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:25:41 -0300

    resolvido erro de sintaxe

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,5 +6,5 @@ def multiplica(num1, num2):
 
 @pytest.mark.parametrize('num1, num2, valoresperado', [0, 0, 0])
 def test_multiplica(num1, num2, valoresperado):
-    assert multiplica(num1, num2) === valoresperado
+    assert multiplica(num1, num2) == valoresperado
 
