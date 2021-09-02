a604e9be030a666c489fab72d6e17f1e22811ff7
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:43:33 -0300

    refatorado funcao multiplica

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,7 +1,9 @@
 import pytest
 
 def multiplica(valor1, valor2):
-    return 0
+    resultado_multiplicacao = valor1 * valor2
+    
+    return resultado_multiplicacao
 
 @pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0), [1, 1, 1]])
 def test_multiplica_com_parametros_validos(valor1, valor2, valor_esperado):
