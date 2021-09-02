b4c911eb8d042458d15be6f696f1329fe9013951
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:36:04 -0300

    criado teste de multiplicacao

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,6 +5,10 @@ def multiplica(valor1, valor2):
 
 
 @pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
-def test_multiplica(valor1, valor2, valor_esperado):
+def test_multiplica(valor1, valor2):
     assert multiplica(valor1, valor1) == valor_esperado
 
+@pytest.mark.parametrize('valor1, valor2, valor_esperado', [(0, 0, 0)])
+def test_multiplica_com_parametros_validos(valor1, valor2):
+    assert valor1 * valor2 == valor_esperado
+    assert multiplica(valor1, valor2) == valor_esperado
