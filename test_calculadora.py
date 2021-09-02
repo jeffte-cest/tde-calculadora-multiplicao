961aed6a3b1c4d17b545c1b6c73cbdc765503d75
jeffte-cest <90008844+jeffte-cest@users.noreply.github.com>
2021-09-02 19:23:24 -0300

    criado primeiro teste

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,1 +1,10 @@
+import pytest
+
+def multiplica(num1, num2):
+    pass
+
+
+@pytest.mark.parametrize('num1, num2, valoresperado', [0, 0, 0])
+def test_multiplica(num1, num2, valoresperado):
+    assert multiplica(num1, num2) === valoresperado
 
